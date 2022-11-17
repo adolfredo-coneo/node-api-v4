b8bcd7f76f9d0049a298a44d6ffb93488f7f7824
Adolfredo Coneo <ado.coneo@gmail.com>
2022-11-17 11:05:56 -0500

    add express-validator

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,7 @@
+import { UPDATE_STATUS } from '@prisma/client';
 import { Router } from 'express';
+import { body, check, oneOf } from 'express-validator';
+import { checkValidationResults } from './modules/middlewares';
 
 const router = Router();
 
@@ -10,8 +13,18 @@ router.get('/product', (req, res) => {
   res.json({ message: req.body.user });
 });
 router.get('/product/:id', (req, res) => {});
-router.put('/product/:id', (req, res) => {});
-router.post('/product', (req, res) => {});
+router.put(
+  '/product/:id',
+  body('name').isString(),
+  checkValidationResults,
+  (req, res) => {}
+);
+router.post(
+  '/product',
+  body('name').isString(),
+  checkValidationResults,
+  (req, res) => {}
+);
 router.delete('/product/:id', (req, res) => {});
 
 /**
@@ -19,8 +32,22 @@ router.delete('/product/:id', (req, res) => {});
  */
 router.get('/update', (req, res) => {});
 router.get('/update/:id', (req, res) => {});
-router.put('/update/:id', (req, res) => {});
-router.post('/update', (req, res) => {});
+router.put(
+  '/update/:id',
+  body('title').optional().isString(),
+  body('body').optional().isString(),
+  body('status').isIn(Object.values(UPDATE_STATUS)),
+  body('version').optional(),
+  checkValidationResults,
+  (req, res) => {}
+);
+router.post(
+  '/update',
+  body('title').isString(),
+  body('body').isString(),
+  checkValidationResults,
+  (req, res) => {}
+);
 router.delete('/update/:id', (req, res) => {});
 
 /**
@@ -28,8 +55,21 @@ router.delete('/update/:id', (req, res) => {});
  */
 router.get('/updatepoint', (req, res) => {});
 router.get('/updatepoint/:id', (req, res) => {});
-router.put('/updatepoint/:id', (req, res) => {});
-router.post('/updatepoint', (req, res) => {});
+router.put(
+  '/updatepoint/:id',
+  body('name').optional().isString(),
+  body('description').optional().isString(),
+  checkValidationResults,
+  (req, res) => {}
+);
+router.post(
+  '/updatepoint',
+  body('name').isString(),
+  body('description').isString(),
+  body('updateId').isString(),
+  checkValidationResults,
+  (req, res) => {}
+);
 router.delete('/updatepoint/:id', (req, res) => {});
 
 export default router;
